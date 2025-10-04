857170a627041103c96136d87fb659c7e5a6d92a
240034 <240034@astanait.edu.kz>
2025-10-05 01:50:55 +0500

    Update BenchmarkRunner.java

--- src/main/java/cli/BenchmarkRunner.java
+++ src/main/java/cli/BenchmarkRunner.java
@@ -1,1 +1,43 @@
-public class BenchmarkRunner {}
+package cli;
+
+import algorithms.ShellSort;
+import algorithms.ShellSort.GapSequence;
+import metrics.PerformanceTracker;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class BenchmarkRunner {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter array size:");
+        int n = scanner.nextInt();
+
+        System.out.println("Choose gap sequence: SHELL, KNUTH, SEDGEWICK");
+        String seqInput = scanner.next().toUpperCase();
+
+        GapSequence sequence;
+        try {
+            sequence = GapSequence.valueOf(seqInput);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Invalid sequence, using SHELL by default.");
+            sequence = GapSequence.SHELL;
+        }
+
+        int[] arr = new int[n];
+        Random rand = new Random();
+        for (int i = 0; i < n; i++) arr[i] = rand.nextInt(100000);
+
+        ShellSort sorter = new ShellSort(GapSequence.SHELL);
+
+        long startTime = System.nanoTime();
+        sorter.sort(arr, sequence);
+        long endTime = System.nanoTime();
+
+        PerformanceTracker tracker = sorter.getTracker();
+
+        System.out.println("Sorting time: " + ((endTime - startTime)/1e6) + " ms");
+        System.out.println(tracker);
+    }
+}
